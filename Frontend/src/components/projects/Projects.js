6587eca2f1f0b14c1d321592a1099cd6269e1124
Soham Patil <145631825+Soham271@users.noreply.github.com>
2025-03-30 21:56:11 +0530

    Update Projects.js
    Add new project details

--- Frontend/src/components/projects/Projects.js
+++ Frontend/src/components/projects/Projects.js
@@ -16,19 +16,31 @@ const Projects = () => {
         />
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 xl:gap-14">
+        {/* Job Auto Filler Project */}
         <ProjectsCard
           title="JOB_AUTO FILLER"
-          des="JOB_AUTO FILLER is a cutting-edge web application designed to streamline the process of resume data extraction and auto-filling. By leveraging advanced resume parsing technology, the project extracts relevant information from resumes and stores it securely in a database. With seamless integration through a browser extension, AutoResume allows for the automatic population of input fields on any web page, making data entry faster, more efficient, and error-free."
+          des="JOB_AUTO FILLER is a cutting-edge web application designed to streamline the process of resume data extraction and auto-filling. It extracts relevant information from resumes and stores it securely in a database. The browser extension then auto-fills input fields on websites, making data entry faster and error-free."
           src={projectOne}
-          githubLink="https://github.com/Arnav-NK/Job_Application_Auto-filler" // Pass GitHub link
-          websiteLink="https://your-website-link.com" // Pass website link (if any)
+          githubLink="https://github.com/Arnav-NK/Job_Application_Auto-filler"
+          liveLink="https://job-autofiller-demo.netlify.app"
         />
+
+        {/* Zamato Project */}
         <ProjectsCard
           title="Zamato"
           des="Zamato is a feature-rich food delivery app that brings the convenience of restaurant meals directly to your doorstep, offering seamless ordering and real-time tracking."
+          src={projectTwo}
+          githubLink="https://github.com/Soham271/Zamto"
+          liveLink="https://zamato-demo.netlify.app"
+        />
+
+        {/* Online Code Editor Project */}
+        <ProjectsCard
+          title="Online-Code-Editor"
+          des="Code Sync is a real-time collaborative code editor that enables multiple users to write and edit code together seamlessly. It supports multiple languages, features room-based collaboration, and ensures real-time synchronization with Socket.io, making coding with peers effortless and efficient."
           src={projectThree}
-          githubLink="https://github.com/Soham271/Zamto" // Pass GitHub link
-          websiteLink="https://your-website-link.com" // Pass website link (if any)
+          githubLink="https://github.com/Soham271/Online-code-Editor"
+          liveLink="https://online-code-sync-demo.netlify.app"
         />
       </div>
     </section>
