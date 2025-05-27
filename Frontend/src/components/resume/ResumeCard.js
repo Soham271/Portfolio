37e63181fcca5da5f6367bcf2b40e7c0c8a288fd
Soham Patil <145631825+Soham271@users.noreply.github.com>
2025-05-27 21:22:34 +0530

    Update ResumeCard.js

--- Frontend/src/components/resume/ResumeCard.js
+++ Frontend/src/components/resume/ResumeCard.js
@@ -2,29 +2,29 @@ import React from "react";
 
 const ResumeCard = ({ title, subTitle, result, des }) => {
   return (
-    <div className="w-full h-1/3 group flex">
-      <div className="w-10 h-[6px] bgOpacity mt-16 relative">
-        <span className="absolute w-5 h-5 rounded-full -top-2 -left-3 flex justify-center items-center bg-black bg-opacity-60">
-          <span className="w-3 h-3 rounded-full bg-bodyColor inline-flex group-hover:bg-designColor duration-300"></span>
+    <div className="w-full flex group">
+      <div className="w-10 h-[6px] bg-designColor mt-16 relative">
+        <span className="absolute w-5 h-5 rounded-full -top-2 -left-3 flex justify-center items-center bg-gray-800">
+          <span className="w-3 h-3 rounded-full bg-designColor inline-flex group-hover:bg-white transition-colors duration-300"></span>
         </span>
       </div>
-      <div className="w-full bg-black bg-opacity-20 hover:bg-opacity-30 duration-300  rounded-lg p-4 lgl:px-10 flex flex-col justify-center gap-6 lgl:gap-10 shadow-shadowOne">
-        <div className="flex flex-col lgl:flex-row justify-between gap-4 lgl:gap-0 lgl:items-center">
+      <div className="w-full bg-gray-800 rounded-lg p-6 flex flex-col justify-center gap-4 shadowOne hover:bg-gray-700 transition-all duration-300">
+        <div className="flex flex-col lg:flex-row justify-between gap-4 lg:items-center">
           <div>
-            <h3 className="text-xl md:text-2xl font-semibold group-hover:text-white duration-300">
+            <h3 className="text-xl md:text-2xl font-semibold text-white group-hover:text-designColor transition-colors duration-300">
               {title}
             </h3>
-            <p className="text-sm mt-2 text-gray-400 group-hover:text-white duration-300">
+            <p className="text-sm mt-2 text-gray-300 group-hover:text-white">
               {subTitle}
             </p>
           </div>
           <div>
-            <p className="px-4 py-2 text-designColor bg-black bg-opacity-25 rounded-lg flex justify-center items-center shadow-shadowOne text-sm font-medium">
+            <p className="px-4 py-2 bg-designColor text-white rounded-lg text-sm font-medium shadowOne">
               {result}
             </p>
           </div>
         </div>
-        <p className="text-sm md:text-base font-medium text-gray-400 group-hover:text-gray-300 duration-300">
+        <p className="text-sm md:text-base text-gray-300 group-hover:text-gray-100">
           {des}
         </p>
       </div>
