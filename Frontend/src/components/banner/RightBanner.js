650d3a862b9589cca1fd0645ac75f0816dfb489f
Soham Patil <145631825+Soham271@users.noreply.github.com>
2025-05-27 21:23:30 +0530

    Update RightBanner.js

--- Frontend/src/components/banner/RightBanner.js
+++ Frontend/src/components/banner/RightBanner.js
@@ -1,17 +1,21 @@
-import React from 'react'
+import React from "react";
+import { motion } from "framer-motion";
 import { bannerImg } from "../../assets/index";
 
 const RightBanner = () => {
   return (
-    <div className="w-full lgl:w-1/2 flex justify-center items-center relative">
-      <img
-        className="w-[300px] h-[400px] lgl:w-[500px] lgl:h-[680px] z-10"
+    <div className="w-full lg:w-1/2 flex justify-center items-center relative">
+      <motion.img
+        initial={{ opacity: 0, scale: 0.8 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{ duration: 0.8 }}
+        className="w-[300px] h-[400px] lg:w-[500px] lg:h-[600px] rounded-2xl object-cover z-10 shadowOne"
         src={bannerImg}
-        alt="bannerImg"
+        alt="Soham Patil"
       />
-      <div className="absolute bottom-0 w-[350px] h-[300px] lgl:w-[500px] lgl:h-[500px] bg-gradient-to-r from-[#1e2024] to-[#202327] shadow-shadowOne flex justify-center items-center"></div>
+      <div className="absolute bottom-0 w-[320px] h-[280px] lg:w-[460px] lg:h-[460px] bg-gradient-to-r from-[#1e2024] to-[#202327] rounded-2xl shadowOne"></div>
     </div>
   );
-}
+};
 
-export default RightBanner
+export default RightBanner;
