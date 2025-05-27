2825380d55af02e74f7888a7d6e61c7292be8b52
Soham Patil <145631825+Soham271@users.noreply.github.com>
2025-05-27 21:22:59 +0530

    Update Education.js

--- Frontend/src/components/resume/Education.js
+++ Frontend/src/components/resume/Education.js
@@ -6,62 +6,36 @@ const Education = () => {
   return (
     <motion.div
       initial={{ opacity: 0 }}
-      animate={{ opacity: 1, transition: { duration: 0.5 } }}
-      className="w-full flex flex-col lgl:flex-row gap-10 lgl:gap-20"
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.5 }}
+      className="w-full flex flex-col lg:flex-row gap-10 lg:gap-20"
     >
-      {/* part one */}
-      <div>
-        <div className="py-6 lgl:py-12 font-titleFont flex flex-col gap-4">
-          <h2 className="text-3xl md:text-4xl font-bold">Education Quality</h2>
+      <div className="w-full">
+        <div className="py-6 lg:py-12 font-titleFont flex flex-col gap-4">
+          <h2 className="text-3xl md:text-4xl font-bold text-white">
+            Education
+          </h2>
         </div>
-        <div className="mt-6 lgl:mt-14 w-full h-[1000px] border-l-[6px] border-l-black border-opacity-30 flex flex-col gap-10">
+        <div className="mt-6 lg:mt-14 w-full border-l-[6px] border-designColor flex flex-col gap-10">
           <ResumeCard
-            title="Integrated B.tech and M.tech in Information Technology"
+            title="Integrated B.Tech and M.Tech in Information Technology"
             subTitle="IIIT Gwalior (2023 - 2028)"
-            result="7.53/10"
-            des="The training provided by universities in order to prepare people to work in various sectors of the economy or areas of culture."
+            result="7.78/10"
+            des="Comprehensive training to excel in various sectors of technology and innovation."
           />
           <ResumeCard
-            title="Higher Secondary School  Certificate"
-            subTitle="Dada  Saheb Namdas devidas Bhole College (2021-2023) "
+            title="Higher Secondary School Certificate"
+            subTitle="Dada Saheb Namdas Devidas Bhole College (2021-2023)"
             result="84.83%"
-            des="Higher education is tertiary education leading to award of an academic degree. Higher education, also called post-secondary education."
+            des="Tertiary education preparing for advanced academic and professional pursuits."
           />
           <ResumeCard
             title="Central Board of Secondary Education"
             subTitle="Tapti Public School (2009 - 2020)"
             result="90.2%"
-            des="Secondary education or post-primary education covers two phases on the International Standard Classification of Education scale."
+            des="Foundational education covering key academic disciplines."
           />
         </div>
-      </div>
-      {/* part Two */}
-
-      <div>
-        {/* <div className="py-6 lgl:py-12 font-titleFont flex flex-col gap-4">
-          <p className="text-sm text-designColor tracking-[4px]">2010 - 2022</p>
-          <h2 className="text-3xl md:text-4xl font-bold">Job Experience</h2>
-        </div> */}
-        {/* <div className="mt-6 lgl:mt-14 w-full h-[1000px] border-l-[6px] border-l-black border-opacity-30 flex flex-col gap-10">
-          <ResumeCard
-            title="Sr. Software Engineer"
-            subTitle="Google Out Tech - (2017 - Present)"
-            result="USA"
-            des="Google's hiring process is an important part of our culture. Googlers care deeply about their teams and the people who make them up."
-          />
-          <ResumeCard
-            title="Web Developer & Trainer"
-            subTitle="Apple Developer Team - (2012 - 2016)"
-            result="MALAYSIA"
-            des="A popular destination with a growing number of highly qualified homegrown graduates, it's true that securing a role in Malaysia isn't easy."
-          />
-          <ResumeCard
-            title="Front-end Developer"
-            subTitle="Nike - (2020 - 2011)"
-            result="Oman"
-            des="The Oman economy has grown strongly over recent years, having transformed itself from a producer and innovation-based economy."
-          />
-        </div> */}
       </div>
     </motion.div>
   );
